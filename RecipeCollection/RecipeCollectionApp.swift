bc3fc91b73be714dbdaf29ff4ebf5160d72730e2
Thomas Romay <romaythomas@gmail.com>
2025-12-11 13:28:35 +0000

    Integrate feature modules
    Added Core, CoreDB, CoreUI, and RecipesFeature Swift packages to the project and updated the app entry point to use RecipesFactory for the main view. Removed ContentView and all UI test files, as well as related references in the project configuration.

--- RecipeCollection/RecipeCollectionApp.swift
+++ RecipeCollection/RecipeCollectionApp.swift
@@ -5,13 +5,29 @@
 //  Created by Thomas Romay on 11/12/2025.
 //
 
+import Core
+import CoreDB
+import RecipesFeature
 import SwiftUI
 
 @main
 struct RecipeCollectionApp: App {
+    private let factory: RecipesFactoryProtocol
+    static let key: String = ""
+    init() {
+        guard !Self.key.isEmpty else {
+            fatalError("API key must be set before running the app")
+        }
+
+        let remoteService = RecipesRemoteService(apiKey: Self.key)
+        let localService = RecipesLocalService()
+        let repository = RecipesRepository(remote: remoteService, local: localService)
+        self.factory = RecipesFactory(repository: repository)
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            factory.makeRecipesListView()
         }
     }
 }
--- RecipeCollectionTests/RecipeCollectionTests.swift
+++ RecipeCollectionTests/RecipeCollectionTests.swift
@@ -5,11 +5,10 @@
 //  Created by Thomas Romay on 11/12/2025.
 //
 
-import XCTest
 @testable import RecipeCollection
+import XCTest
 
 final class RecipeCollectionTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -32,5 +31,4 @@ final class RecipeCollectionTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
