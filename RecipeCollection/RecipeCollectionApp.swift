bbd6897f417bb2634313ff142f29654fd67ac5f9
Thomas Romay <romaythomas@gmail.com>
2025-12-11 09:39:55 +0000

    Initial Commit

---
+++ RecipeCollection/RecipeCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeCollectionApp.swift
+//  RecipeCollection
+//
+//  Created by Thomas Romay on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
